2c1cf7adc200e24bef89c885f9207b7d8b2a7980
Oleg <music.wave.form7@gmail.com>
2020-03-25 23:47:34 +0300

    movies serach actions tests

---
+++ app/containers/MoviesSearch/tests/actions.test.ts
@@ -0,0 +1,36 @@
+import { action } from 'typesafe-actions';
+import {
+  loadMovies,
+  changeMovieName,
+  moviesLoadingError,
+  moviesLoaded,
+} from '../actions';
+import ActionTypes from '../constants';
+import { MovieListItemType } from '../components/MovieListItem/types';
+
+describe('Movies Search Actions', () => {
+  it('loadMovies', () => {
+    const expectedResult = action(ActionTypes.LOAD_MOVIES, { page: 2 });
+
+    expect(loadMovies(2)).toEqual(expectedResult);
+  });
+  it('changeMovieName', () => {
+    const expectedResult = action(ActionTypes.CHANGE_MOVIE_NAME, 'title');
+
+    expect(changeMovieName('title')).toEqual(expectedResult);
+  });
+  it('moviesLoadingError', () => {
+    const expectedResult = action(ActionTypes.LOAD_MOVIES_ERROR, 'error');
+
+    expect(moviesLoadingError('error')).toEqual(expectedResult);
+  });
+  it('moviesLoaded', () => {
+    const pl = {
+      Search: [],
+      totalResults: 0,
+    };
+    const expectedResult = action(ActionTypes.LOAD_MOVIES_SUCCESS, pl);
+
+    expect(moviesLoaded(pl)).toEqual(expectedResult);
+  });
+});
